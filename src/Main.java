1c889dcd39aead7870c2e9c175db1f1ce50c9a13
Ninel0204 <116366686+Ninel0204@users.noreply.github.com>
2022-10-23 15:50:57 +0300

    Delete src directory

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int [] sales=new int[3];
-        SalesManager salesManager=new SalesManager(sales);
-        System.out.println(salesManager.max());
-
-    }
-}
